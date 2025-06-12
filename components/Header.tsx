4203cd31a59c9b3cbe0bceb116f89c764748fd68
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-12 03:39:11 -0700

    pages commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,13 @@ export default function Header() {
     // [#F44336] 
     // fixed / absolute / middle outer-w transparent?
     <div className='flex items-center justify-between py-4 px-6 absolute z-100 bg-[#FAFAFA] text-[#D32F2F] rounded-b-lg w-full border-b-2  border-b-[#D32F2F]'> 
-      <div>
+      <div className=''>
         <Logo />
       </div>
       <div className='flex gap-4 font-semibold '>
           <div className='cursor-pointer transition-all duration-300 hover:scale-105 hover:backdrop-blur-3xl '>Home</div>
           <div className='cursor-pointer transition-all duration-300 hover:scale-105 hover:backdrop-blur-3xl '>Services</div>
-          <div className='cursor-pointer transition-all duration-300 hover:scale-105 hover:backdrop-blur-3xl ' >Aboutus</div>
+          <div className='cursor-pointer transition-all duration-300 hover:scale-105 hover:backdrop-blur-3xl ' >About us</div>
       </div>
       <div>Login</div>
   </div>
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className='flex gap-1 items-center '>
-        <div className='flex items-center '>
+    <div className='flex items-center '>
             {/* <i className="fa-solid fa-feather-pointed "></i>
             <i className="fa-solid fa-feather-pointed rotate-290 -ml-1 "></i>
             <span className=''>A</span>
@@ -13,13 +12,10 @@ export default function Logo() {
             <Image
             src="/logo.jpg"
             alt="Company Logo"
-            className='rounded-full h-9 w-9 object-cover '
+            className='rounded-full h-9 w-9 object-cover border-2 transition-all duration-300 hover:scale-125 cursor-pointer  border-[#D32F2F] '
             width={120}
             height={60}         
             />
-        </div>
-        <span>
-        </span> 
     </div>
   )
 }
