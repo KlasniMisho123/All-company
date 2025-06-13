f4a1b7af76e4911542712a79e817d48b99a2e0a9
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-13 14:25:38 -0700

    navbar active

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,24 +1,29 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Image from "next/image";
 import Link from 'next/link';
 
+type NavbarProps = {
+  selectedNav: string;
+  setSelectedNav: React.Dispatch<React.SetStateAction<string>>;
+  handleSelectedNav: (nav: string) => void;
+};
 
-export default function Logo() {
+export default function Logo(props:NavbarProps) {
+    const {selectedNav, setSelectedNav, handleSelectedNav } = props
+    
   return (
     <Link className='flex items-center '
     href={"/"}
+    onClick={() => handleSelectedNav("/")}
     >
-            {/* <i className="fa-solid fa-feather-pointed "></i>
-            <i className="fa-solid fa-feather-pointed rotate-290 -ml-1 "></i>
-            <span className=''>A</span>
-            <span className=''>ll</span> */}
-            <Image
-            src="/logo.jpg"
-            alt="Company Logo"
-            className='rounded-full min-h-11 min-w-11 h-11 w-11 object-cover border-2 transition-all duration-300 hover:scale-125 cursor-pointer border-[#D32F2F] '
-            width={120}
-            height={60}         
-            />
+      <Image
+      src="/logo.jpg"
+      alt="Company Logo"
+      className='rounded-full min-h-11 min-w-11 h-11 w-11 object-cover border-2 transition-all duration-300 hover:scale-125 cursor-pointer border-[#D32F2F] '
+      width={120}
+      height={60}         
+      />
     </Link>
   )
 }
