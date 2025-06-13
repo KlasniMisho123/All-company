06b4827e85b15fa42147f168485c8624f8cfed4d
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-13 08:23:38 -0700

    sidenavbar animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,17 +7,12 @@ import Navbar from './Navbar'
 export default function Header() {
 
   const [isSideNavActive, setIsSideNavActive ] = useState(false)
-  // [#D32F2F]
-  // [#A10000]
-  // [#F44336] 
-  // fixed / absolute / middle outer-w transparent?
 
   function handleSideNav():void {
     setTimeout(()=>{
       setIsSideNavActive(!isSideNavActive)
     },1000)
   }
-
   return (
     <div>
       <div className='flex items-center justify-between p-2 sm:py-4 sm:px-6 absolute z-100 bg-[#FAFAFA] text-[#D32F2F] rounded-b-lg w-full border-b-2 border-b-[#D32F2F] '> 
@@ -36,12 +31,11 @@ export default function Header() {
         {/* <div className='flex sm:hidden'> */}
 
         <div className='flex'>
-          <button className='flex flex-col items-center cursor-pointer transition-all duration-300 hover:backdrop-blur-3xl font-semibold group tuncate '
-          onClick={handleSideNav}
-          >
-               <i className="fa-solid fa-minus text-lg -mt-2"></i>
-               <i className="fa-solid fa-minus text-lg -mt-3"></i>
-               <i className="fa-solid fa-minus text-lg -mt-3"></i>
+          <button className='flex flex-col items-center cursor-pointer transition-all duration-300 hover:backdrop-blur-3xl font-semibold group tuncate group '
+          onClick={handleSideNav}>
+               <i className={"fa-solid fa-minus text-lg -mt-3 group-hover:opacity-90 " + (isSideNavActive ? "menu-line-1 " : " ")}></i>
+               <i className={"fa-solid fa-minus text-lg -mt-3 group-hover:opacity-90 " + (isSideNavActive ? "menu-line-2 " : " ")}></i>
+               <i className={"fa-solid fa-minus text-lg -mt-3 group-hover:opacity-90 " + (isSideNavActive ? "menu-line-3 " : " ")}></i>
           </button>
         </div>
 
