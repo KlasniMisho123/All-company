bbc3c187cdc49ed709fbb3140223233a2253b891
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-13 14:19:09 -0700

    navbar active

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,8 @@ import Link from 'next/link'
 
 export default function Header() {
   const [selectedNav, setSelectedNav ] = useState("")
-  const [isSideNavActive, setIsSideNavActive ] = useState(false)
-  const [isMenuActive, setIsMenuActive ] = useState(false)
+  const [isSideNavActive, setIsSideNavActive ] = useState(true)
+  const [isMenuActive, setIsMenuActive ] = useState(true)
   const [aniEnabled, setAniEnabled] = useState(false)
 
   
@@ -29,8 +29,8 @@ export default function Header() {
 
 
   return (
-    <div className='flex flex-col '>
-      <div className='flex items-center justify-between gap-2 p-2 sm:py-4 sm:px-6 z-100 bg-[#FAFAFA] text-[#D32F2F] rounded-b-lg w-full border-b-2 border-b-[#D32F2F] min-w-fit '> 
+    <div className='relative w-full z-50 '>
+      <div className='relative flex items-center justify-between gap-2 p-2 sm:py-4 sm:px-6 z-100 bg-[#FAFAFA] text-[#D32F2F] rounded-b-lg w-full border-b-2 border-b-[#D32F2F] min-w-fit '> 
         <Logo />
         <div className='hidden sm:flex'>
           <Navbar selectedNav={selectedNav} setSelectedNav={setSelectedNav} handleSelectedNav={handleSelectedNav} />
@@ -43,7 +43,7 @@ export default function Header() {
           </div>
         </div>
 
-        <div className='flex items-center sm:hidden sm:items-start cursor-pointer px-1 '
+        <div className='flex items-center sm:hidden sm:items-start cursor-pointer p-2 '
         onClick={handleSideNav}
         >
           <button className='flex flex-col items-center  transition-all duration-300 hover:backdrop-blur-3xl font-semibold tuncate hover:opacity-90  '
@@ -53,19 +53,19 @@ export default function Header() {
                <i className={"fa-solid fa-minus text-lg -mt-3 " + ( aniEnabled ? isSideNavActive ? "menu-line-3 " : "rev-menu-line-3 " : " ")}></i>
           </button>
         </div>
-
       </div>
+
       {isMenuActive && (
-       <div className={`transition-all duration-2000 ease-in-out overflow-hidden bg-[#FFF5F5] 
+       <div className={`absolute  w-full top-full z-40  transition-all duration-2000 ease-in-out overflow-hidden bg-[#FFF5F5] 
            border-b-2 border-b-[#D32F2F] rounded  ${isSideNavActive? ' drop-down-navbar -mt-2 ' : ' rev-drop-down-navbar ' }`}>
              <div className="flex flex-col p-4 pt-6 text-gray-700 font-medium space-y-1">
                 <Link
                   href="/"
                   onClick={() => setSelectedNav('/')}
-                  className={`flex items-center gap-2 px-4 py-3 rounded-md transition-colors duration-300 ${
+                  className={`flex items-center gap-2 px-4 py-3 rounded-md transition-all duration-500 hover:translate-x-3  ${
                     selectedNav === '/' 
                       ? 'bg-red-100 text-[#D32F2F] font-semibold' 
-                      : 'hover:bg-gray-100 hover:text-[#D32F2F]'
+                      : 'hover:bg-blue-100  hover:text-[#D32F2F]'
                   }`}
                 >
                   <i className="fa-solid fa-house text-[#D32F2F]"></i>
@@ -75,31 +75,31 @@ export default function Header() {
                 <Link
                   href="/services"
                   onClick={() => setSelectedNav('/services')}
-                  className={`flex items-center gap-2 px-4 py-3 rounded-md transition-colors duration-300 ${
+                  className={`flex items-center gap-2 px-4 py-3 rounded-md transition-all duration-500 group hover:translate-x-3 ${
                     selectedNav === '/services' 
                       ? 'bg-red-100 text-[#D32F2F] font-semibold' 
-                      : 'hover:bg-gray-100 hover:text-[#D32F2F]'
+                      : 'hover:bg-blue-100 hover:text-[#D32F2F]'
                   }`}
                 >
-                  <i className="fa-solid fa-gear text-[#D32F2F]"></i>
+                  <i className="fa-solid fa-gear text-[#D32F2F] transition-all duration-500 group-hover:rotate-270 "></i>
                   Services
                 </Link>
 
                 <Link
                   href="/aboutus"
                   onClick={() => setSelectedNav('/aboutus')}
-                  className={`flex items-center gap-2 px-4 py-3 rounded-md transition-colors duration-300 ${
+                  className={`flex items-center gap-2 px-4 py-3 rounded-md transition-all duration-500 hover:translate-x-3 ${
                     selectedNav === '/aboutus' 
                       ? 'bg-red-100 text-[#D32F2F] font-semibold' 
-                      : 'hover:bg-gray-100 hover:text-[#D32F2F]'
+                      : 'hover:bg-blue-100  hover:text-[#D32F2F]'
                   }`}
                 >
                   <i className="fa-solid fa-users text-[#D32F2F]"></i>
                   About Us
                 </Link>
 
-                <div className="flex items-center gap-2 px-4 py-3 rounded-md transition-colors duration-300 hover:bg-gray-100 hover:text-[#D32F2F] cursor-pointer">
-                  <i className="fa-solid fa-right-to-bracket text-[#D32F2F]"></i>
+                <div className="flex items-center gap-2 px-4 py-3 rounded-md transition-all duration-500 hover:bg-blue-100 hover:text-[#D32F2F] cursor-pointer hover:translate-x-3">
+                  <i className="fa-solid fa-right-to-bracket text-[#D32F2F]  "></i>
                   Login
                 </div>
               </div>
