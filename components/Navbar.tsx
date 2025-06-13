ee78279de47b6d91f828f8f4e266d28d8f73701f
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-13 08:57:26 -0700

    navbar active

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,29 +3,36 @@ import React, { useEffect, useState } from 'react'
 
 
 export default function Navbar() {
-  const [activeNav, setActiveNav ] = useState("")
+  const [aselectedNav, setSelectedNav ] = useState("")
 
+  function handleSelectedNav(nav:string):void {
+    setSelectedNav(nav)
+  }
+  
     useEffect(() => {
       const path = window.location.pathname
-      setActiveNav(path)
+      setSelectedNav(path)
     },[])
 
   return (
     <div className='flex gap-4 font-semibold '>
         <Link className={`flex gap-2 items-center cursor-pointer transition-all duration-300  border-1 rounded-full px-4 py-2 shadow-md hover:shadow-none hover:bg-[#D32F2F]
-         hover:text-white ` + (activeNav==="/" ? "bg-[#D32F2F] text-white" : " ")}
-        href={'/'}>
+         hover:text-white ` + (aselectedNav==="/" ? "bg-[#D32F2F] text-white" : " ")}
+          href={'/'}
+          onClick={() => handleSelectedNav("/")}>
             <i className="fa-solid fa-house"></i>
             <span> Home </span>
         </Link>
         <Link className={`flex gap-2 items-center cursor-pointer transition-all duration-300  border-1 rounded-full px-4 py-2 shadow-md hover:shadow-none hover:bg-[#D32F2F]
-         hover:text-white ` + (activeNav==="/services" ? "bg-[#D32F2F] text-white " : " ")}
-        href={'/services'}>
+         hover:text-white ` + (aselectedNav==="/services" ? "bg-[#D32F2F] text-white " : " ")}
+        href={'/services'}
+        onClick={() => handleSelectedNav("/services")}>
             <span> Services </span>
         </Link>
         <Link className={`flex gap-2 items-center cursor-pointer transition-all duration-300  border-1 rounded-full px-4 py-2 shadow-md hover:shadow-none hover:bg-[#D32F2F]
-         hover:text-white truncate ` + (activeNav==="/aboutus" ? "bg-[#D32F2F] text-white" : " ")} 
-        href={'/aboutus'}>
+         hover:text-white truncate ` + (aselectedNav==="/aboutus" ? "bg-[#D32F2F] text-white" : " ")} 
+        href={'/aboutus'}
+        onClick={() => handleSelectedNav("/aboutus")}>
             <span> About us </span>
         <i className="fa-solid fa-feather-pointed"></i>
         </Link>
