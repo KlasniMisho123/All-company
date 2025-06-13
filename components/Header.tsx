2a10619cdbd51cccc412b3b7b0bbbda3254e5e6c
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-13 08:38:39 -0700

    sidenavbar animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from './Logo'
 // import Link from 'next/link'
 import Navbar from './Navbar'
@@ -8,6 +8,7 @@ export default function Header() {
 
   const [isSideNavActive, setIsSideNavActive ] = useState(false)
   const [isMenuActive, setIsMenuActive ] = useState(false)
+  const [aniEnabled, setAniEnabled] = useState(false)
 
   function handleSideNav():void {
     setIsMenuActive(!isMenuActive)
@@ -15,6 +16,13 @@ export default function Header() {
       setIsSideNavActive(!isSideNavActive)
     },1000)
   }
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setAniEnabled(true)
+    },1000)
+  },[])
+
   return (
     <div>
       <div className='flex items-center justify-between p-2 sm:py-4 sm:px-6 absolute z-100 bg-[#FAFAFA] text-[#D32F2F] rounded-b-lg w-full border-b-2 border-b-[#D32F2F] '> 
@@ -32,12 +40,12 @@ export default function Header() {
 
         {/* <div className='flex sm:hidden'> */}
 
-        <div className='flex'>
-          <button className='flex flex-col items-center cursor-pointer transition-all duration-300 hover:backdrop-blur-3xl font-semibold group tuncate group '
+        <div className='flex items-center '>
+          <button className='flex flex-col items-center cursor-pointer transition-all duration-300 hover:backdrop-blur-3xl font-semibold tuncate hover:opacity-90 '
           onClick={handleSideNav}>
-               <i className={"fa-solid fa-minus text-lg -mt-3 group-hover:opacity-90 " + (isMenuActive ? " menu-line-1 " : " rev-menu-line-1 ")}></i>
-               <i className={"fa-solid fa-minus text-lg -mt-3 group-hover:opacity-90 " + (isMenuActive ? " menu-line-2 " : " rev-menu-line-2 ")}></i>
-               <i className={"fa-solid fa-minus text-lg -mt-3 group-hover:opacity-90 " + (isMenuActive ? " menu-line-3 " : " rev-menu-line-3 ")}></i>
+               <i className={"fa-solid fa-minus text-lg " + (isMenuActive && aniEnabled ? "menu-line-1 " : "rev-menu-line-1 ")}></i>
+               <i className={"fa-solid fa-minus text-lg -mt-3 " + (isMenuActive && aniEnabled ? "menu-line-2 " : "rev-menu-line-2 ")}></i>
+               <i className={"fa-solid fa-minus text-lg -mt-3 " + (isMenuActive && aniEnabled ? "menu-line-3 " : "rev-menu-line-3 ")}></i>
           </button>
         </div>
 
