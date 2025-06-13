d15e3fcb115ce984c8884d62d21cee7ba0524586
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-13 07:45:30 -0700

    header commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,17 +10,30 @@ export default function Header() {
     // [#F44336] 
     // fixed / absolute / middle outer-w transparent?
     <div>
-        <div className='flex items-center justify-between px-2 py-4 md:px-6 absolute z-100 bg-[#FAFAFA] text-[#D32F2F] rounded-b-lg w-full border-b-2  border-b-[#D32F2F]'> 
-          <Logo />
+      <div className='flex items-center justify-between p-2 sm:py-4 sm:px-6 absolute z-100 bg-[#FAFAFA] text-[#D32F2F] rounded-b-lg w-full border-b-2 border-b-[#D32F2F] '> 
+        <Logo />
+        <div className='hidden sm:flex'>
           <Navbar />
-          <div className='flex items-center gap-2 cursor-pointer text-lg transition-all duration-300 hover:backdrop-blur-3xl font-semibold group tuncate '>
+        </div>
+
+        <div className='hidden sm:flex'>
+          <div className='flex items-center gap-2 cursor-pointer transition-all duration-300 hover:backdrop-blur-3xl font-semibold group tuncate '>
             <span> Login </span>
             < i className="fa-solid fa-right-to-bracket transition-all duration-300 group-hover:translate-x-1 "/>
           </div>
         </div>
+
+        <div className='flex sm:hidden'>
+          <div className='flex flex-col items-center cursor-pointer transition-all duration-300 hover:backdrop-blur-3xl font-semibold group tuncate '>
+               <i className="fa-solid fa-minus text-lg -mt-2 "></i>
+               <i className="fa-solid fa-minus text-lg -mt-3"></i>
+               <i className="fa-solid fa-minus text-lg -mt-3"></i>
+          </div>
+        </div>
+
+      </div>
       {/* Dropdown Navbar  */}
       <div className='bg-green-500'>
-        
       </div>
     </div>
   )
