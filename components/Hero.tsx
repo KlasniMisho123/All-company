267b31559c7a2bb3a276692a90c38881a4bf7a26
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-15 00:47:38 -0700

    hero card commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,22 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='h-[100vh]'>Hero</div>
+    <div className='h-[100vh]'>
+      <section className="mx-16 mt-8 flex gap-4 bg-pink-200">
+        <div className="flex-1 bg-blue-200">
+          <h4>Social Media For Learners</h4>
+          <h2> Order and Get Customized Designs <br/>
+          Decorations and more...
+          </h2>
+          <h4> We Will Unique Designs For You</h4>
+          <button> Join </button>
+          <div className="text-gray-700" > Already Have Account? <span className="font-bold "> Log In </span> </div>
+        </div>
+        <div className="flex-1 bg-green-200">
+          Grid
+        </div>
+      </section>
+
+    </div>
   )
 }
