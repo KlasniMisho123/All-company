99f2ddf0adc3597fdd539d69e2e03d9f50978e72
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-15 01:49:15 -0700

    hero card commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
-import Footer from "@/components/Footer";
+import { Inter, Urbanist } from 'next/font/google';
 import React from 'react'
+
+const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
+const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] })
 
 export default function Hero() {
   return (
@@ -7,14 +10,16 @@ export default function Hero() {
       <section className="mx-30 my-12 flex gap-2 ">
         <div className="flex-1 flex flex-col gap-4">
           <h4 className="text-lg text-[var(--error-color)] font-semibold ">Designs That Speak for You</h4>
-          <h2 className="text-6xl max-w-[600px] font-semibold leading-tight">
-          <span className="pl-1 pr-4 border-2 border-blue-500 text-blue-500 rounded-r-full rounded-l-none mb-4"> Get Custom Visuals </span> 
+          <h2 className={"text-6xl max-w-[600px] font-semibold leading-tight "  + urbanist.className}>
+          <span className={"pl-1 pr-4 border-2 border-blue-500 text-blue-500 rounded-r-full rounded-l-none mb-4 truncate "}> Get Custom Visuals </span><br/>
             for Social Media,
             Branding, Events, and More
           </h2>
-          <h4 className="text-lg font-semibold "> Crafted <span className="text-blue-500 "> Uniquely </span> . Designed Just for You.</h4>
+          <h4 className="text-lg font-semibold "> <i className="fa-solid fa-hammer text-blue-500 forging "></i> Crafted <span className="text-blue-500 "> Uniquely </span> . Designed Just for You.</h4>
           <div className="pt-4">
-            <button className="text-xl text-[var(--error-color)] border-2 border-[var(--error-color)] font-semibold cursor-pointer rounded-full px-8 py-4 hover:bg-red-500 transition-all duration-300 hover:text-white "> Order Now </button>
+            <button className="text-xl text-white bg-[var(--error-color,_#D32F2F)] border-2 border-[var(--error-color,_#D32F2F)] font-semibold cursor-pointer rounded-full px-8 py-4 hover:bg-white hover:text-[var(--error-color,_#D32F2F)] transition-all duration-300 shadow-sm">
+              Order Now
+            </button>
             <div className="text-gray-700 mt-2">
               Already have an account?&nbsp;
               <span className="relative group font-bold cursor-pointer inline-block">
@@ -24,11 +29,14 @@ export default function Hero() {
             </div>
           </div>
         </div>
-        <div className="flex-1 bg-green-200">
-          Grid
-        </div>
+        
       </section>
 
     </div>
   )
 }
+
+
+{/* <div className="flex-1 bg-green-200">
+  Grid
+</div> */}
