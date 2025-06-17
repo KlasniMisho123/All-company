0d73b4d04e9ce2042f616f9e81ae64d10cd3d3a6
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-17 09:13:59 -0700

    heropage commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,8 +6,9 @@ const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function Hero() {
   return (
-    <div className=''>
-      <section className="mx-30 my-12 flex flex-col md:flex-row  gap-2 ">
+    <div className='pb-12'>
+      {/* Dark Background */}
+      <section className="mx-30 my-12 flex flex-col md:flex-row  gap-2  ">
         <div className="flex-1 flex flex-col gap-4">
           <h4 className="text-lg text-[var(--error-color)] font-semibold ">Designs That Speak for You</h4>
           <h2 className={"text-6xl max-w-[600px] font-semibold leading-tight "  + urbanist.className}>
@@ -17,7 +18,7 @@ export default function Hero() {
           </h2>
           <h4 className="text-lg font-semibold "> <i className="fa-solid fa-hammer text-blue-500 forging "></i> Crafted <span className="text-blue-500 "> Uniquely </span> . Designed Just for You.</h4>
           <div className="pt-4">
-            <button className="text-xl text-white bg-[var(--error-color,_#D32F2F)] border-2 border-[var(--error-color,_#D32F2F)] font-semibold cursor-pointer rounded-full px-8 py-4 hover:bg-white hover:text-[var(--error-color,_#D32F2F)] transition-all duration-300 shadow-sm">
+            <button className="text-xl text-white bg-[var(--error-color,_#D32F2F)] border-2 border-[var(--error-color,_#D32F2F)] font-semibold cursor-pointer rounded-full px-8 py-3 hover:bg-white hover:text-[var(--error-color,_#D32F2F)] transition-all duration-300 shadow-sm">
               Order Now
             </button>
             <div className="text-gray-700 mt-2">
@@ -29,10 +30,23 @@ export default function Hero() {
             </div>
           </div>
         </div>
-        {/* Clippath Displayed pictures */}
         <HeroGrid />
       </section>
+      {/* Page padding Can be Set on parent component aswell */}
+      <section className='mx-30 my-20'> 
+        <h2 className='text-center text-4xl font-bold text-[var(--dark-red-color)] '> What We Offer </h2> 
 
+        <div className='flex mt-8'>
+          <div className='bg-red-300 h-[200px] w-[200px] '> 
+
+          </div>
+
+          <div> Card 2</div>
+          <div> Card 3</div>
+          <div> Card 4</div>
+        </div>
+
+      </section>
     </div>
   )
 }
