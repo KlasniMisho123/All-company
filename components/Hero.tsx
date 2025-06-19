c7873e5145c60cf53f04406bacee98b46cf9184e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-19 04:19:39 -0700

    dark theme

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import { Urbanist } from 'next/font/google';
 import React from 'react'
 import HeroGrid from './HeroGrid';
 import ServicesCard from './ServicesCard';
+import Link from 'next/link';
 
 const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -40,36 +41,40 @@ export default function Hero() {
       </section>
 
       <section className='mx-30 my-10 px-20 py-10 w-[90%] h-[400px] shadow-sm rounded bg-gradient-to-r from-slate-900 to-slate-800 flex justify-between '>
-        <div className='flex flex-col gap-4  '>
-          <div className="relative inline-block group ">
+        <div className=''>
+          <div className="relative inline-block group mt-2 ">
             <h1 className="text-3xl md:text-4xl font-bold ">
               All Design
               <div className="h-1 w-0 bg-red-500 transition-all duration-500 group-hover:w-full"></div>
             </h1>
           </div>
-          <p className="text-gray-200 max-w-2xl text-center md:text-left text-lg leading-relaxed">
+          <p className="text-gray-200 max-w-2xl text-center md:text-left text-lg leading-relaxed mt-4">
             Where creativity meets strategy—explore our comprehensive design solutions. <br />
             See how we transform visions into brands
           </p>
-          <div className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-2 mt-6'>
             <div className='flex items-center gap-2 group '>
               <i className='fa-solid fa-lightbulb text-2xl p-2 px-3 transition-all duration-500 border-2 border-transparent rounded-full group-hover:border-yellow-400  group-hover:bg-white group-hover:text-yellow-400 shadow-sm '></i>
-              <span>Client&apos;s Vision</span>
+              <span className='text-yellow-400' >Client&apos;s Vision</span>
             </div>
-            <div className='flex items-center gap-4 text-2xl p-2 px-3 transition-all duration-500 border-2 border-transparent rounded-full group-hover:border-yellow-400  group-hover:bg-white group-hover:text-yellow-400 shadow-sm '>
-              <i className='fa-solid fa-cubes text-2xl '></i>
-              <span>Quality Product</span>
+            <div className='flex items-center gap-2 group'>
+              <i className='fa-solid fa-cubes text-2xl text-2xl p-2  transition-all duration-500 border-2 border-transparent rounded-full group-hover:border-teal-500  group-hover:bg-white group-hover:text-teal-500 shadow-sm '></i>
+              <span className='text-teal-500' >Quality Product</span>
             </div>
-            <div className='flex items-center gap-4 text-2xl p-2 px-3 transition-all duration-500 border-2 border-transparent rounded-full group-hover:border-yellow-400  group-hover:bg-white group-hover:text-yellow-400 shadow-sm '>
-              <i className='fa-solid fa-handshake text-2xl'></i>
-              <span>Trusted Partnership</span>
+            <div className='flex items-center gap-2 group'>
+              <i className='fa-solid fa-handshake text-2xl text-2xl p-2 px-1.5 transition-all duration-500 border-2 border-transparent rounded-full group-hover:border-blue-500 group-hover:bg-white group-hover:text-blue-500 shadow-sm '></i>
+              <span className='text-blue-500 ' >Trusted Partnership</span>
             </div>
           </div>
         </div>
-        <div className=''>
-          <button className="text-xl text-white bg-[var(--error-color,_#D32F2F)] border-2 border-[var(--error-color,_#D32F2F)] font-semibold cursor-pointer rounded-full px-8 py-3 hover:bg-white hover:text-[var(--error-color,_#D32F2F)] transition-all duration-300 shadow-sm">
-            Order Now
-          </button>
+        <div className='self-center '>
+          <Link
+           className="flex items-center gap-2 text-xl text-white bg-[var(--error-color,_#D32F2F)] border-2 border-[var(--error-color,_#D32F2F)] 
+           font-semibold cursor-pointer rounded-full px-8 py-3 hover:bg-white hover:text-[var(--error-color,_#D32F2F)] transition-all duration-300 shadow-sm overflow-hidden plane-flight  "
+           href={'/aboutus'}
+           >
+            About Us <i className="fa-solid fa-paper-plane "></i>
+          </Link>
         </div>
       </section>
 
