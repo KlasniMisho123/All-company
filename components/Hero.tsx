52dd0a2d6a7ca3059a7fffc452c1e03023dcc16b
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-19 12:53:56 -0700

    working stages card

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import HeroGrid from './HeroGrid';
 import ServicesCard from './ServicesCard';
 import Link from 'next/link';
+import WorkStages from './WorkingStagesCard';
 
 const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -116,78 +117,8 @@ export default function Hero() {
           />
         </div>
       </section>
-      <section className='mx-30 my-10 rounded-xl  p-4 py-10 '>
-        <h4 className='text-2xl'> Work Process </h4>
-        <div className='flex justify-between mt-2 gap-4 '>
-          <div className='flex-1 flex flex-col gap-8 mt-4 '>
-            <h2 className='text-4xl font-semibold '> Our business   <span className='text-[var(--error-color)]'> Process Road </span> </h2>
-            <p className='text-gray-100 mt-2 max-w-[80%]'>
-              <span className='text-lg'> 🎨 </span> From the first pencil sketch to the final brand guidelines, our design process transforms raw ideas into iconic visual identities. 
-              Each phase blends strategic thinking with artistic mastery—keeping you engaged at every turn, while crafting results that don&apos;t just meet briefs,
-              but redefine categories.
-            </p>
-            <div className='p-2 px-4 border-2 bg-yellow-400 border-yellow-400 rounded-xl '>01. Research Phase</div>
-            <div className='p-2 px-4 border-2 bg-blue-400 border-blue-400 rounded-xl '>02. Visual Exploration</div>
-            <div className='p-2 px-4 border-2 bg-red-400 border-red-400 rounded-xl '>03. System Design</div>
-            <div className='p-2 px-4 border-2 bg-green-400 border-green-400 rounded-xl '>04. System Design</div>
-          </div>
-
-          <div className='flex-1 grid grid-cols-2 gap-8 mt-4'>
-
-            <div className='flex flex-col w-full bg-white h-[350px] rounded-xl shadow-lg duration-300 transition-all overflow-hidden hover:border-yellow-400 border-3 group relative'>
-              <div className='flex-1 flex items-center'>
-                <i className="fa-solid fa-lightbulb text-8xl text-yellow-400/60 ml-4 fa-bounce"></i>
-                <h1 className='absolute ml-10 text-black text-3xl '>DISCOVERY</h1>
-              </div>
-              <div className='flex-1 p-4 pt-0 text-gray-700 group-hover:text-yellow-400 transition-all'>
-                We decode your brand DNA through research, moodboards, and strategy sessions.
-              </div>
-              <div className='absolute bottom-4 right-4 text-yellow-400 opacity-0 group-hover:opacity-100 duration-300 transition-all'>
-                01
-              </div>
-            </div>
-
-            <div className='flex flex-col w-full bg-white h-[350px] rounded-xl shadow-lg duration-300 transition-all overflow-hidden hover:border-blue-400 border-3 group relative mt-6'>
-              <div className='flex-1 flex items-center'>
-                <i className="fa-solid fa-pen-fancy text-8xl text-blue-400/60 ml-4 group-hover:rotate-12 transition-transform"></i>
-                <h1 className='absolute ml-10 text-black text-3xl '>CONCEPT</h1>
-              </div>
-              <div className='flex-1 p-4 pt-0 text-gray-700 group-hover:text-blue-400 transition-all'>
-                Transforming insights into 3 distinct visual directions for your brand.
-              </div>
-              <div className='absolute bottom-4 right-4 text-blue-400 opacity-0 group-hover:opacity-100 duration-300 transition-all'>
-                02
-              </div>
-            </div>
-            
-            <div className='flex flex-col w-full bg-white h-[350px] rounded-xl shadow-lg duration-300 transition-all overflow-hidden hover:border-red-400 border-3 group relative'>
-              <div className='flex-1 flex items-center'>
-                <i className="fa-solid fa-fingerprint text-8xl text-red-400/60 ml-4 group-hover:rotate-45 transition-transform"></i>
-                <h1 className='absolute ml-10 text-black text-3xl'>IDENTITY CORE</h1>
-              </div>
-              <div className='flex-1 p-4 pt-0 text-gray-700 group-hover:text-red-400 transition-all'>
-                Crafting logo, color, and typography systems that become unmistakably yours.
-              </div>
-              <div className='absolute bottom-4 right-4 text-red-400 opacity-0 group-hover:opacity-100 duration-300 transition-all'>
-                03
-              </div>
-            </div>
-
-            <div className='flex flex-col w-full bg-white h-[350px] rounded-xl shadow-lg duration-300 transition-all overflow-hidden hover:border-green-400 border-3 group relative '>
-              <div className='flex-1 flex items-center'>
-                <i className="fa-solid fa-box-open text-8xl text-green-400/60 ml-4 group-hover:animate-bounce"></i>
-                <h1 className='absolute ml-10 text-black text-3xl '>DELIVERY</h1>
-              </div>
-              <div className='flex-1 p-4 pt-0 text-gray-700 group-hover:text-green-400 transition-all'>
-                Complete brand packages with guidelines and implementation support.
-              </div>
-              <div className='absolute bottom-4 right-4 text-green-400 opacity-0 group-hover:opacity-100 duration-300 transition-all'>
-                04
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <WorkStages />
+     
       {/*Highlighted Cleint Logo Infinite Scroll */}
       {/*String Sides/Advantages Infinite Scroll */}
     </div>
