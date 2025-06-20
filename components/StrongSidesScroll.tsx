1a7ecb70c9ba10ed2eb663bf7571dd6adfb7727d
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-20 11:38:38 -0700

    working stages

--- components/StrongSidesScroll.tsx
+++ components/StrongSidesScroll.tsx
@@ -1,10 +1,42 @@
 import React from 'react'
 
+import { Exo_2, Inter } from 'next/font/google'
+
+const exo2 = Exo_2({subsets: ["latin"],weight: ["400", "700"]});
+const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
+
 export default function StrongSidesScroll() {
   return (
-    <section className='my-10 bg-red-300 py-4 flex'>
-        ss
-         <div className=''> </div>
+    <section className='flex items-center justify-evenly my-10 bg-gradient-to-r from-slate-900 to-slate-800 py-4 px-2 flex overflow-hidden '>
+        <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full '>
+            <i className='fa-solid fa-bullseye text-red-400 '> </i>
+            <span className={` ` + exo2.className}> Client Targeted </span>
+        </div>
+
+         <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full'>
+            <i className="fa-solid fa-flask text-blue-400"></i>
+            <span className={inter.className}>Brand Alchemists</span>
+        </div>
+
+        <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+            <i className="fa-solid fa-gauge-high text-teal-400 "></i>
+            <span className={` ` + inter.className}> Turbo Workflow  </span>
+        </div>
+        
+         <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+            <i className="fa-solid fa-user text-pink-400"></i>
+            <span className={` ` + inter.className}> Dedicated Designers </span>
+        </div>
+
+         <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+            <i className="fa-solid fa-fingerprint text-cyan-400"></i>
+            <span className={` ` + inter.className}> Unique Designs </span>
+        </div>
+
+         <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+            <i className="fa-solid fa-medal text-yellow-400"></i>
+            <span className={` ` + inter.className}> Quality </span>
+        </div>
     </section>
   )
 }
