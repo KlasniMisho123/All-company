51f2d45c84a09001236f25ae528d1cbf72d538f0
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-21 08:37:19 -0700

    button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Button() {
   return (
-    <button className="relative self-start border-2 border-red-500 rounded-full overflow-hidden cursor-pointer px-6 py-3 button-clipath bg-white ">
+    <Link className="relative self-start border-2 border-red-500 rounded-full overflow-hidden cursor-pointer px-6 py-3 button-clipath bg-white "
+    href={"/projects"}>
         <div className=" absolute inset-0 bg-[var(--error-color)] "></div>
-        
         <span className="relative z-10 font-semibold ">Check Out Our Projects</span>
-    </button>
+    </Link>
   )
 }
