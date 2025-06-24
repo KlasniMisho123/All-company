321d3ad7ac8e88172d485f07dade730a09251896
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-24 10:25:00 -0700

    infinite horisontal animation

--- components/StrongSidesScroll.tsx
+++ components/StrongSidesScroll.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
 export default function StrongSidesScroll() {
   return (
     <section className=' my-10 bg-gradient-to-r from-slate-900 to-slate-800 py-4 px-2  overflow-hidden strong-sides w-full '>
-        <div className='flex items-center justify-evenly w-full animate-infinite-scroll '>
+        <div className="flex items-center gap-10 animate-infinite-scroll w-max">
             <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full truncate'>
                 <i className='fa-solid fa-bullseye text-red-400 '> </i>
                 <span className={` ` + exo2.className}> Client Targeted </span>
@@ -40,8 +40,8 @@ export default function StrongSidesScroll() {
             </div>
 
             {/* Dublicates */}
-
-            {/* <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full truncate'>
+    
+            <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full truncate'>
                 <i className='fa-solid fa-bullseye text-red-400 '> </i>
                 <span className={` ` + exo2.className}> Client Targeted </span>
             </div>
@@ -49,14 +49,14 @@ export default function StrongSidesScroll() {
             <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full'>
                 <i className="fa-solid fa-flask text-blue-400"></i>
                 <span className={inter.className}>Brand Alchemists</span>
-            </div> */}
+            </div>
 
-            {/* <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
                 <i className="fa-solid fa-gauge-high text-teal-400 "></i>
                 <span className={` ` + inter.className}> Turbo Workflow  </span>
-            </div> */}
+            </div>
             
-             {/* <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+             <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
                 <i className="fa-solid fa-user text-pink-400"></i>
                 <span className={` ` + inter.className}> Dedicated Designers </span>
             </div>
@@ -69,7 +69,38 @@ export default function StrongSidesScroll() {
             <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
                 <i className="fa-solid fa-medal text-yellow-400"></i>
                 <span className={` ` + inter.className}> Quality </span>
-            </div> */}
+            </div>
+            {/* Dublicates */}
+    
+            <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full truncate'>
+                <i className='fa-solid fa-bullseye text-red-400 '> </i>
+                <span className={` ` + exo2.className}> Client Targeted </span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full'>
+                <i className="fa-solid fa-flask text-blue-400"></i>
+                <span className={inter.className}>Brand Alchemists</span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+                <i className="fa-solid fa-gauge-high text-teal-400 "></i>
+                <span className={` ` + inter.className}> Turbo Workflow  </span>
+            </div>
+            
+             <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+                <i className="fa-solid fa-user text-pink-400"></i>
+                <span className={` ` + inter.className}> Dedicated Designers </span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+                <i className="fa-solid fa-fingerprint text-cyan-400"></i>
+                <span className={` ` + inter.className}> Unique Designs </span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+                <i className="fa-solid fa-medal text-yellow-400"></i>
+                <span className={` ` + inter.className}> Quality </span>
+            </div>
         </div>
     </section>
   )
