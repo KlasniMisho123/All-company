59ec163e451911597db86f40a9dd9a8e2ef53d8a
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-24 11:08:13 -0700

    infinite horisontal animation

--- components/StrongSidesScroll.tsx
+++ components/StrongSidesScroll.tsx
@@ -6,132 +6,47 @@ const exo2 = Exo_2({subsets: ["latin"],weight: ["400", "700"]});
 const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function StrongSidesScroll() {
+    const CardElements = (
+        <>
+            <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full truncate hover:bg-red-500 group transition-colors duration-200'>
+                <i className='fa-solid fa-bullseye text-red-400 group-hover:text-white transition-colors duration-200'></i>
+                <span className={`${exo2.className} group-hover:text-white transition-colors duration-200`}>Client Targeted</span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full truncate hover:bg-blue-500 group transition-colors duration-200'>
+            <i className="fa-solid fa-flask text-blue-400 group-hover:text-white transition-colors duration-200"></i>
+            <span className={inter.className}>Brand Alchemists</span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full truncate hover:bg-teal-500 group transition-colors duration-200'>
+            <i className="fa-solid fa-gauge-high text-teal-400 group-hover:text-white transition-colors duration-200"></i>
+            <span className={inter.className}>Turbo Workflow</span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full truncate hover:bg-pink-500 group transition-colors duration-200'>
+            <i className="fa-solid fa-user text-pink-400 group-hover:text-white transition-colors duration-200"></i>
+            <span className={inter.className}>Dedicated Designers</span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full truncate hover:bg-cyan-500 group transition-colors duration-200'>
+            <i className="fa-solid fa-fingerprint text-cyan-400 group-hover:text-white transition-colors duration-200"></i>
+            <span className={inter.className}>Unique Designs</span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full truncate hover:bg-yellow-500 group transition-colors duration-200'>
+            <i className="fa-solid fa-medal text-yellow-400 group-hover:text-white transition-colors duration-200"></i>
+            <span className={inter.className}>Quality</span>
+            </div>
+        </>
+    );
+
   return (
     <section className=' my-10 bg-gradient-to-r from-slate-900 to-slate-800 py-4 px-2  overflow-hidden strong-sides w-full '>
         <div className="flex items-center gap-10 animate-infinite-scroll w-max">
-            <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full truncate'>
-                <i className='fa-solid fa-bullseye text-red-400 '> </i>
-                <span className={` ` + exo2.className}> Client Targeted </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full truncate'>
-                <i className="fa-solid fa-flask text-blue-400"></i>
-                <span className={inter.className}>Brand Alchemists</span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full truncate'>
-                <i className="fa-solid fa-gauge-high text-teal-400 "></i>
-                <span className={` ` + inter.className}> Turbo Workflow  </span>
-            </div>
-            
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full truncate'>
-                <i className="fa-solid fa-user text-pink-400"></i>
-                <span className={` ` + inter.className}> Dedicated Designers </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full truncate'>
-                <i className="fa-solid fa-fingerprint text-cyan-400"></i>
-                <span className={` ` + inter.className}> Unique Designs </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full truncate'>
-                <i className="fa-solid fa-medal text-yellow-400"></i>
-                <span className={` ` + inter.className}> Quality </span>
-            </div>
-
-            {/* Dublicates */}
-    
-            <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full truncate'>
-                <i className='fa-solid fa-bullseye text-red-400 '> </i>
-                <span className={` ` + exo2.className}> Client Targeted </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full'>
-                <i className="fa-solid fa-flask text-blue-400"></i>
-                <span className={inter.className}>Brand Alchemists</span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-gauge-high text-teal-400 "></i>
-                <span className={` ` + inter.className}> Turbo Workflow  </span>
-            </div>
-            
-             <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-user text-pink-400"></i>
-                <span className={` ` + inter.className}> Dedicated Designers </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-fingerprint text-cyan-400"></i>
-                <span className={` ` + inter.className}> Unique Designs </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-medal text-yellow-400"></i>
-                <span className={` ` + inter.className}> Quality </span>
-            </div>
-            {/* Dublicates */}
-    
-            <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full truncate'>
-                <i className='fa-solid fa-bullseye text-red-400 '> </i>
-                <span className={` ` + exo2.className}> Client Targeted </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full'>
-                <i className="fa-solid fa-flask text-blue-400"></i>
-                <span className={inter.className}>Brand Alchemists</span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-gauge-high text-teal-400 "></i>
-                <span className={` ` + inter.className}> Turbo Workflow  </span>
-            </div>
-            
-             <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-user text-pink-400"></i>
-                <span className={` ` + inter.className}> Dedicated Designers </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-fingerprint text-cyan-400"></i>
-                <span className={` ` + inter.className}> Unique Designs </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-medal text-yellow-400"></i>
-                <span className={` ` + inter.className}> Quality </span>
-            </div>
-            {/* Dublicates */}
-    
-            <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full truncate'>
-                <i className='fa-solid fa-bullseye text-red-400 '> </i>
-                <span className={` ` + exo2.className}> Client Targeted </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full'>
-                <i className="fa-solid fa-flask text-blue-400"></i>
-                <span className={inter.className}>Brand Alchemists</span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-gauge-high text-teal-400 "></i>
-                <span className={` ` + inter.className}> Turbo Workflow  </span>
-            </div>
-            
-             <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-user text-pink-400"></i>
-                <span className={` ` + inter.className}> Dedicated Designers </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-fingerprint text-cyan-400"></i>
-                <span className={` ` + inter.className}> Unique Designs </span>
-            </div>
-
-            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
-                <i className="fa-solid fa-medal text-yellow-400"></i>
-                <span className={` ` + inter.className}> Quality </span>
-            </div>
+            {CardElements}
+            {CardElements}
+            {CardElements}
+            {CardElements}
         </div>
     </section>
   )
