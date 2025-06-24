cb7277620ab43d43b1f1342afd783a28fde224c9
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-24 10:41:28 -0700

    infinite horisontal animation

--- components/StrongSidesScroll.tsx
+++ components/StrongSidesScroll.tsx
@@ -101,6 +101,37 @@ export default function StrongSidesScroll() {
                 <i className="fa-solid fa-medal text-yellow-400"></i>
                 <span className={` ` + inter.className}> Quality </span>
             </div>
+            {/* Dublicates */}
+    
+            <div className='flex gap-2 items-center border-2 px-4 p-1 rounded-full truncate'>
+                <i className='fa-solid fa-bullseye text-red-400 '> </i>
+                <span className={` ` + exo2.className}> Client Targeted </span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full'>
+                <i className="fa-solid fa-flask text-blue-400"></i>
+                <span className={inter.className}>Brand Alchemists</span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+                <i className="fa-solid fa-gauge-high text-teal-400 "></i>
+                <span className={` ` + inter.className}> Turbo Workflow  </span>
+            </div>
+            
+             <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+                <i className="fa-solid fa-user text-pink-400"></i>
+                <span className={` ` + inter.className}> Dedicated Designers </span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+                <i className="fa-solid fa-fingerprint text-cyan-400"></i>
+                <span className={` ` + inter.className}> Unique Designs </span>
+            </div>
+
+            <div className='flex gap-2 items-center border-2 border-white px-4 p-1 rounded-full '>
+                <i className="fa-solid fa-medal text-yellow-400"></i>
+                <span className={` ` + inter.className}> Quality </span>
+            </div>
         </div>
     </section>
   )
