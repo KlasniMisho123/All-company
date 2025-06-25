e1aa3a311abcc63e6dd071b5a057b691c8cd4a1c
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-25 09:29:09 -0700

    companies commit

--- components/ClientCompanies.tsx
+++ components/ClientCompanies.tsx
@@ -3,55 +3,36 @@ import Image from 'next/image'
 
 export default function ClientCompanies() {
   return (
-    // <div className='flex justify-evenly items-center bg-gradient-to-r from-slate-900 to-slate-800 px-2 py-12 text-white ' >
-    
-    <div className='flex justify-evenly items-center bg-white px-2 py-12 text-white ' >
-        {/* <div className='max-w-[20%] h-[90px] transition-all duration-300 grayscale-100 hover:grayscale-0 hover:scale-110 '>
+    <div className='flex justify-evenly items-center bg-gradient-to-r from-slate-900 to-slate-800 px-2 py-8 ' >  
+
+        <div className='max-w-[20%] h-[120px] transition-all duration-300 grayscale-100 hover:grayscale-0 scale-110 opacity-50  hover:scale-140 hover:opacity-100 '>
             <img
-            src={"/companies/sura.jpg"}
-            className='object-cover w-full h-full min-w-[125px]'
+            src={"/companies/noctvrna.png"}
+            className='object-cover w-full h-full min-w-[150px]'
             />
         </div>
-        <div className='max-w-[20%] h-[90px] transition-all duration-300 grayscale-100 hover:grayscale-0 hover:scale-110 '>
+        <div className='max-w-[20%] h-[120px] transition-all duration-300 grayscale-100 hover:grayscale-0 opacity-50 hover:scale-130 hover:opacity-100 '>
             <img
-            src={"/companies/adBlueVega.jpg"}
-            className='object-fit w-full h-full min-w-[125px] '
+            src={"/companies/need.png"}
+            className='object-cover w-full h-full min-w-[150px]'
             />
         </div>
-        <div className='max-w-[20%] h-[90px] transition-all duration-300 grayscale-100 hover:grayscale-0 hover:scale-110 '>
+        <div className='max-w-[20%] h-[120px] transition-all duration-300 grayscale-100 hover:grayscale-0 opacity-50 scale-150  hover:scale-180 hover:opacity-100 '>
             <img
-            src={"/companies/sulfix.jpg"}
-            className='object-cover w-full h-full min-w-[125px]'
-            /> 
-        </div>*/}
-        <div className='max-w-[20%] h-[90px] transition-all duration-300 grayscale-100 hover:grayscale-0 hover:scale-110 '>
-            <img
-            src={"/companies/clinton.png"}
-            className='object-cover w-full h-full min-w-[125px]'
+            src={"/companies/dune.png"}
+            className='object-cover w-full h-full min-w-[150px]'
             />
         </div>
-        <div className='max-w-[20%] h-[90px] transition-all duration-300 grayscale-100 hover:grayscale-0 hover:scale-110 '>
+        <div className='max-w-[20%] h-[120px] transition-all duration-300 grayscale-100 hover:grayscale-0 opacity-50 hover:scale-130 hover:opacity-100 '>
             <img
-            src={"/companies/ramires.png"}
-            className='object-cover w-full h-full min-w-[125px]'
+            src={"/companies/star.png"}
+            className='object-cover w-full h-full min-w-[150px]'
             />
         </div>
-        <div className='max-w-[20%] h-[90px] transition-all duration-300 grayscale-100 hover:grayscale-0 hover:scale-110 '>
+        <div className='max-w-[20%] h-[120px] transition-all duration-300 grayscale-100 hover:grayscale-0 scale-110 opacity-50  hover:scale-140 hover:opacity-100 '>
             <img
-            src={"/companies/clinton.png"}
-            className='object-cover w-full h-full min-w-[125px]'
-            />
-        </div>
-        <div className='max-w-[20%] h-[90px] transition-all duration-300 grayscale-100 hover:grayscale-0 hover:scale-110 '>
-            <img
-            src={"/companies/ramires.png"}
-            className='object-cover w-full h-full min-w-[125px]'
-            />
-        </div>
-        <div className='max-w-[20%] h-[90px] transition-all duration-300 grayscale-100 hover:grayscale-0 hover:scale-110 '>
-            <img
-            src={"/companies/ramires.png"}
-            className='object-cover w-full h-full min-w-[125px]'
+            src={"/companies/superlative.png"}
+            className='object-cover w-full h-full min-w-[150px]'
             />
         </div>
     </div>
