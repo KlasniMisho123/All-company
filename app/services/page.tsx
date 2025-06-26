c0fd68c838aef7a62c5a294c4a3d53c3e8e85418
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-26 14:53:22 -0700

    use Auth commit

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -11,7 +11,12 @@ export default function page() {
       >
         <h1 className='text-6xl font-semibold custom-selection-bg '> Services </h1>
       </div>
-        Services
+      <div className='flex flex-col items-center justify-center my-12 gap-2 '>
+        <h5 className='text-gray-300 text-lg '>What Can We Do?</h5>
+        <h2 className='text-5xl '>OUR SERVICES <span className='text-red-500'> OVERVIEW </span> </h2>
+        <p className='text-gray-300 max-w-[40%] text-center text-xl mt-4'> A factory, manufacturing plant or a production plant is an industrial site,
+          usually consisting of buildings and machinery </p>
+      </div>
     </div>
   )
 }
