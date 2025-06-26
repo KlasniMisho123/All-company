d412593f27d7bbfa4bfd05cdd5619b7dd965e3a8
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-26 14:27:01 -0700

    use Auth commit

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 
-import { useAuth } from '@/context/AuthContext'
 import React from 'react'
 
 export default function page() {
 
-  const { userData } = useAuth()
-  
   return (
-    <div>{userData}</div>
+    <div>
+      <div className='h-[50vh] -mt-1 bg-cover bg-center flex items-center justify-center'
+      style={{backgroundImage:"url(https://xtratheme.com/elementor/factory/wp-content/uploads/sites/8/2018/09/slider-industrial-2.jpg)"}}
+      >
+        <h1 className='text-6xl font-semibold custom-selection-bg '> Services </h1>
+      </div>
+        Services
+    </div>
   )
 }
