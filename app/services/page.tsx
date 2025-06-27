b938ab780f06119bc476953c2c685c884a061799
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-27 03:23:07 -0700

    servises commit

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -9,7 +9,12 @@ export default function page() {
       <div className='h-[50vh] -mt-1 bg-cover bg-center flex items-center justify-center'
       style={{backgroundImage:"url(https://xtratheme.com/elementor/factory/wp-content/uploads/sites/8/2018/09/slider-industrial-2.jpg)"}}
       >
-        <h1 className='text-6xl font-semibold custom-selection-bg '> Services </h1>
+        
+        <div className='flex flex-col text-6xl font-semibold custom-selection-bg text-center  text-white '>
+          <h2 className='bg-[var(--light-red)] py-3'> Our Company </h2>
+          <h2 className=''> Services </h2>
+        </div>
+
       </div>
       <div className='flex flex-col items-center justify-center my-12 gap-2 '>
         <h5 className='text-gray-300 text-lg '>What Can We Do?</h5>
