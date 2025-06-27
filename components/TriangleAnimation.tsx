f900e5a82a70efcbde2bd1b5557723414d447513
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-27 09:03:59 -0700

    triangle animation commit

--- components/TriangleAnimation.tsx
+++ components/TriangleAnimation.tsx
@@ -2,23 +2,46 @@
 import React, { useEffect, useState } from 'react'
 
 export default function TriangleAnimation() {
-    const [randomNumbers, setRandomNumbers ] = useState([])
+    const [randomNumbers, setRandomNumbers ] = useState<number[]>([])
+
+    const topBars = []
+
+    for(let i=0; i<=5; i++) {
+        topBars.push(
+            <div key={i} className='p-1.5 bg-black h-0.5 rounded-full '></div>
+        )
+    }
 
     function generateRandNumber() {
-        const randomNum = Math.random() * 10 
-
+        const randomNum = [
+            Math.floor(Math.random() * 20 ),
+            Math.floor(Math.random() * 20 ),
+            Math.floor(Math.random() * 20 )
+        ]
         console.log("randomNum: ", randomNum)
+        setRandomNumbers(randomNum)
     }
 
     useEffect(() => {
         generateRandNumber()
     },[])
-
+// do elements in html part. just numbers in arrays?
   return (
     <div className='flex flex-col items-center px-4 gap-2 py-4'>
         <div className='flex-1 flex flex-col ml-6'>
-            <div className='flex justify-center gap-0.5'>
+             <div className='flex justify-center gap-0.5'>
+                 {topBars}
+                {/* {topBars.map((element)=>{
+                    eturn(element)
+                })} */}
+                {/* <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
                 <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
+                <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
+                <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
+                <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
+                <div className='p-1.5 bg-black h-0.5 rounded-full '></div> */}
+            </div>
+            {/*<div className='flex justify-center gap-0.5'>
                 <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
                 <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
                 <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
@@ -30,13 +53,6 @@ export default function TriangleAnimation() {
                 <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
                 <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
                 <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
-                <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
-            </div>
-            <div className='flex justify-center gap-0.5'>
-                <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
-                <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
-                <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
-                <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
             </div>
             <div className='flex justify-center gap-0.5'>
                 <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
@@ -49,7 +65,7 @@ export default function TriangleAnimation() {
             </div>
             <div className='flex justify-center gap-0.5'>
                 <div className='p-1.5 bg-black h-0.5 rounded-full '></div>
-            </div>
+            </div> */}
         </div>
 
          <div className='flex-1 flex flex-col-reverse mr-6'>
