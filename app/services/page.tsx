48bc07accf8135d47a98b6436e6f8995357dcce1
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-27 06:26:49 -0700

    servises commit

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -14,23 +14,23 @@ export default function page() {
       style={{backgroundImage:"url(https://xtratheme.com/elementor/factory/wp-content/uploads/sites/8/2018/09/slider-industrial-2.jpg)"}}
       >
         
-        <div className='flex flex-col text-6xl font-semibold custom-selection-bg text-center  text-white '>
-          <h2 className='bg-[var(--light-red)] py-3'> Our Company </h2>
-          <h2 className=''> Services </h2>
+        <div className='flex flex-col text-6xl font-semibold custom-selection-bg text-center text-white select-none '>
+          <h2 className=''>Our Company</h2>
+          <h2 className=''>Services</h2>
         </div>
 
       </div>
       <section className='flex flex-col items-center pb-[350px]'>
         <div className='flex flex-col items-center justify-center my-12 gap-2 '>
           <h5 className='text-gray-300 text-lg '>What Can We Do?</h5>
-          <h2 className='text-5xl '>OUR SERVICES <span className='text-red-500'> OVERVIEW </span> </h2>
+          <h2 className='text-5xl '>OUR SERVICES <span className='bg-[var(--light-red)] text-white px-1'> OVERVIEW </span> </h2>
           <p className='text-gray-300 max-w-[40%] text-center text-xl mt-4'> A factory, manufacturing plant or a production plant is an industrial site,
             usually consisting of buildings and machinery </p>
         </div>
 
         <div className='flex gap-10 w-[60%] '>
           <div className={'mt-8 flex-1 relative flex flex-col items-center gap-2 h-[53vh] p-4 rounded-lg bg-gradient-to-b from-slate-300 via-slate-400 to-slate-300  shadow-2xl text-black ' + urbanist.className }>
-              <h5 className='text-xl '> Logo design </h5>
+              <h5 className='text-xl '> Logo Design </h5>
               <div className='h-[50%] w-full overflow-hidden border-2 border-slate-400 rounded-xl bg-gradient-to-r from-red-300 to-gray-300 shadow-lg'> 
                 <SquareAnimation />
               </div>
@@ -38,9 +38,9 @@ export default function page() {
                 Crafting impactful and memorable logo designs that capture your brand's identity and leave a lasting impression.
               </p>
 
-              {/* <p className="text-white text-sm leading-relaxed bg-[var(--light-red)] px-1 ">
+              <p className="text-white text-sm leading-relaxed bg-[var(--light-red)] px-1 ">
                 Pricing
-              </p> */}
+              </p>
               <button className='relative flex items-center justify-center w-full border-1 py-1 rounded transition-all duration-300 cursor-pointer group overflow-hidden font-semibold
               border-slate-800 hover:border-[var(--error-color)] bg-slate-800 text-white '>
                 <span className='transition-opacity duration-300 group-hover:opacity-0'>Order Now</span>
@@ -51,14 +51,51 @@ export default function page() {
               </button>
           </div>
 
-          <div className='mt-0 flex-1 p-4 rounded-lg bg-red-300 '>
-            wedding decorations
+          <div className={'mt-0 flex-1 relative flex flex-col items-center gap-2 p-4 rounded-lg bg-gradient-to-b from-slate-300 via-slate-400 to-slate-300  shadow-2xl text-black ' + urbanist.className }>
+              {/* Event Styling  */}
+              <h5 className='text-xl '> Wedding Decor </h5>
+              <div className='h-[50%] w-full overflow-hidden border-2 border-slate-400 rounded-xl bg-gradient-to-r from-red-300 to-gray-300 shadow-lg'> 
+                <SquareAnimation />
+              </div>
+              <p className="text-gray-800 text-sm leading-relaxed ">
+                Elegant, personalized designs that bring your wedding vision to life — from florals to lighting and beyond.
+              </p>
+
+              <p className="text-white text-sm leading-relaxed bg-[var(--light-red)] px-1 ">
+                Pricing
+              </p>
+              <button className='relative flex items-center justify-center w-full border-1 py-1 rounded transition-all duration-300 cursor-pointer group overflow-hidden font-semibold
+              border-slate-800 hover:border-[var(--error-color)] bg-slate-800 text-white '>
+                <span className='transition-opacity duration-300 group-hover:opacity-0'>Order Now</span>
+                <span className="absolute z-0 h-[110%] w-full origin-left scale-x-0 bg-[var(--error-color)] transition-transform duration-500 group-hover:scale-x-100"></span>
+                <div className='absolute flex items-center gap-1 justify-center z-10 inset-x-0 text-center opacity-0 transition-all duration-300 group-hover:opacity-100'>
+                  <span> Excellent Choice </span><i className="fa-regular fa-gem text-lg text-blue-400 "></i>
+                </div>
+              </button>
           </div>
 
-          <div className='mt-4  flex-1 p-4 rounded-lg bg-red-300 '>
-            Product Design
-          </div>
+          <div className={'mt-0 flex-1 relative flex flex-col items-center gap-2 p-4 rounded-lg bg-gradient-to-b from-slate-300 via-slate-400 to-slate-300  shadow-2xl text-black ' + urbanist.className }>
+              <h5 className='text-xl '> Product Design </h5>
+              <div className='h-[50%] w-full overflow-hidden border-2 border-slate-400 rounded-xl bg-gradient-to-r from-red-300 to-gray-300 shadow-lg'> 
+                <SquareAnimation />
+              </div>
+              <p className="text-gray-800 text-sm leading-relaxed ">
+                Your product’s look and feel matters — we craft designs that reflect your brand and connect with customers.
+              </p>
 
+              <p className="text-white text-sm leading-relaxed bg-[var(--light-red)] px-1 ">
+                Pricing
+              </p>
+              <button className='relative flex items-center justify-center w-full border-1 py-1 rounded transition-all duration-300 cursor-pointer group overflow-hidden font-semibold
+              border-slate-800 hover:border-[var(--error-color)] bg-slate-800 text-white '>
+                <span className='transition-opacity duration-300 group-hover:opacity-0'>Order Now</span>
+                <span className="absolute z-0 h-[110%] w-full origin-left scale-x-0 bg-[var(--error-color)] transition-transform duration-500 group-hover:scale-x-100"></span>
+                <div className='absolute flex items-center gap-1 justify-center z-10 inset-x-0 text-center opacity-0 transition-all duration-300 group-hover:opacity-100'>
+                  <span> Excellent Choice </span><i className="fa-regular fa-gem text-lg text-blue-400 "></i>
+                </div>
+              </button>
+          </div>
+            
           {/* <div>
             Addvertisement board Design
           </div>
