4fc73dc9a796c8a723a4c15b8cb5f0be30cf1894
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-30 04:47:46 -0700

    user feed card commit

--- components/UserFeedCard.tsx
+++ components/UserFeedCard.tsx
@@ -14,16 +14,16 @@ export default function UserFeedCard(props:userFeedCardType) {
 
   return (
     <div>
-        <div className='flex flex-col gap-4 border border-blue-500 p-6 rounded-xl min-h-[200px] bg-slate-800 shadow-md'>
-            <div className='bg-blue-500 p-5 rounded-full shadow-inner self-start'>
+        <div className={`flex flex-col gap-4 border ${selectedColor.borderBase} p-6 rounded-xl min-h-[200px] bg-slate-800 shadow-md`}>
+            <div className={`${selectedColor.bg} p-5 rounded-full shadow-inner self-start`}>
                 <i className={`${icon} text-4xl text-white`}></i>
             </div>
             <div className=''>
-                <h4 className={'text-2xl font-semibold text-blue-500 ' + urbanist.className}>{stats} {title}</h4>
+                <h4 className={`text-2xl font-semibold ${selectedColor.text} ` + urbanist.className}>{stats} {title}</h4>
                 <h6 className={'text-gray-300 text-sm ' + urbanist.className}>{subTitle}</h6>
                 <p className='text-gray-400 text-xs mt-2'>{p}</p>
             </div>
-            <Link className='mt-4 px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 transition self-end group overflow-hidden rounded-xl -mr-3 '
+            <Link className={`mt-4 px-4 py-2 ${selectedColor.bg} text-white hover:bg-blue-600 transition self-end group overflow-hidden rounded-xl -mr-3 `}
             href={linkHref}
             >   <i className="fa-solid fa-arrow-up rotate-45 transition-all duration-300 group-hover:translate-x-6 group-hover:-translate-y-6 "></i>  </Link>
         </div>
