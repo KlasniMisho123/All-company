af3118d64d0057eb2571dbdc27df40bb658affeb
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-30 05:52:42 -0700

    user feed card commit

--- components/UserFeed.tsx
+++ components/UserFeed.tsx
@@ -4,10 +4,22 @@ import React, { useEffect, useState } from 'react'
 import Link from "next/link"
 import UserFeedCard from './UserFeedCard';
 
+import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function UserFeed() {
     const [totalYears,SetTotalYears] = useState(Number)
+
+    const data = [
+        { name: '5 Stars', value: 80 },
+        { name: '4 Stars', value: 15 },
+        { name: '3 Stars', value: 3 },
+        { name: '2 Stars', value: 1 },
+        { name: '1 Star', value: 1 }
+    ];
+
+    const COLORS = ['#0ea476', '#34d399', '#fbbf24', '#f97316', '#ef4444'];
 
     useEffect(()=> {
         const year = new Date().getFullYear()
@@ -20,6 +32,7 @@ export default function UserFeed() {
        <div> 
         Feedback Div?
        </div>
+        {/*Dummy Stats */}
        <div className='grid grid-cols-3 justify-evenly gap-4 '>
             <UserFeedCard
                 stats={totalYears}
@@ -30,7 +43,46 @@ export default function UserFeed() {
                 p={'Delivering quality, trust, and innovation since day one.'}
                 linkHref={"/aboutus"}
             />
-            <div className='row-span-2 border-1 border-pink-500 p-4 rounded-lg '>Happy Clients</div>
+
+            <div className='row-span-2 b flex flex-col gap-6 border border-emerald-500 p-6 rounded-xl min-h-[200px] bg-slate-800 shadow-md'>
+                <div className='bg-emerald-500 p-5 rounded-full shadow-inner self-start'>
+                    <i className='fa-solid fa-face-smile-beam  text-4xl text-white'></i>
+                </div>
+                <div className=''>
+                    <h4 className={'text-2xl font-semibold text-emerald-500 ' + urbanist.className}>98% Happy Clients</h4>
+                    <h6 className={'text-gray-300 text-sm ' + urbanist.className}>Our Clients Come First</h6>
+                    <p className='text-gray-400 text-xs mt-2'>We build lasting relationships by prioritizing quality and trust in every project.</p>
+                </div>
+                <div className='mt-2 '> 
+                    <h3 className="mb-4 text-center text-emerald-500 text-xl font-semibold tracking-wide">
+                        4.8 Average Rating
+                    </h3>
+                    <div className='w-full h-64'>
+                        <ResponsiveContainer>
+                            <PieChart>
+                            <Pie
+                                data={data}
+                                dataKey="value"
+                                cx="50%"
+                                cy="50%"
+                                outerRadius={80}
+                                label
+                            >
+                                {data.map((entry, index)=>(
+                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                ))}
+                            </Pie>
+                            <Tooltip />
+                            <Legend />
+                            </PieChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
+                <Link className='mt-4 px-4 py-2 bg-emerald-500 text-white hover:bg-emerald-600 transition self-end group overflow-hidden rounded-xl -mr-3 '
+                href={"/aboutus"}
+                >   <i className="fa-solid fa-arrow-up rotate-45 transition-all duration-300 group-hover:translate-x-6 group-hover:-translate-y-6 "></i>  </Link>
+            </div>
+
             <UserFeedCard
                 stats={150}
                 icon={"fa-solid fa-cube"}
