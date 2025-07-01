62bde10fcf180e1b1f633c8290cb3dc7b88d60d0
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-01 11:32:41 -0700

    user feedback card

--- components/UserFeedbackCard.tsx
+++ components/UserFeedbackCard.tsx
@@ -9,7 +9,7 @@ type userFeedbackInfo = {
 }
 
 export default function UserFeedbackCard(props:userFeedbackInfo) {
-    const {userImage, user, rateing,userFeedback, icon} = props
+    const {userImage, user, rateing, userFeedback, icon} = props
     
   return (
     <div className="flex flex-col gap-4 p-4 bg-slate-800 rounded-lg shadow text-sm text-gray-300">
@@ -17,9 +17,9 @@ export default function UserFeedbackCard(props:userFeedbackInfo) {
             <div className="flex items-center gap-3">
             <div
                 className="h-16 w-16 border-2 border-slate-600 rounded-full bg-center bg-cover"
-                style={{ backgroundImage: "url(/zoe-kravitz.jpg)" }}
+                style={{ backgroundImage: userImage }}
             ></div>
-            <span className="text-xs text-gray-400">userexample@gmail.com</span>
+            <span className="text-xs text-gray-400">{user}</span>
             </div>
 
             <div className="flex gap-1 text-[var(--shiny-yellow)]">
@@ -32,10 +32,9 @@ export default function UserFeedbackCard(props:userFeedbackInfo) {
         </div>
 
         <div className="bg-slate-700 p-3 rounded-md italic text-sm text-gray-300 leading-relaxed max-w-md">
-        “This team truly understood our vision and brought it to life with care and creativity. I highly recommend them.”
-        I highly recommend them.”
+        “{userFeedback}”
         </div>
-        <span className="text-xl"></span>
+        <span className="text-xl">{icon}</span>
     </div>
   )
 }
