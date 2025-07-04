94219b9617aba1c5c280fea2bf07087954401c5d
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-04 08:21:28 -0700

    aboutus commit

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function aboutus() {
   return (
-    <div>aboutus</div>
+    <div>
+      <div className='relative h-[50vh] -mt-1 bg-cover bg-center  w-full '
+      style={{backgroundImage: "url(https://images.pexels.com/photos/17046790/pexels-photo-17046790.jpeg)"}}
+      >
+        <div className='absolute inset-0 w-full h-full z-10 bg-gradient-to-r from-black via-red-300 to-black opacity-25' />
+      </div>
+      header
+    </div>
   )
 }
