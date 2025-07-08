c40c7b99ccc11bd7aa3ba1e096a9be2bcb51f97c
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-08 15:54:08 +0400

    faq component commit

--- components/FaqComponent.tsx
+++ components/FaqComponent.tsx
@@ -1,10 +1,12 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { Playfair_Display } from 'next/font/google';
 
 const playfairDisplay = Playfair_Display({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function FaqComponent() {
+    const [selectedQuestions, setSelectedQuestions] = useState<number[]>([]);
+
 
     const faqContent = [
         ["question1 ?", "answer1 !"],
@@ -14,23 +16,38 @@ export default function FaqComponent() {
         ["question5 ?", "answer5 !"]
     ];
 
+    function selectQuestions(index:number) {
+        setSelectedQuestions(prev => [...prev, index])
+    }
+
   return (
     <div className='hadow-lg bg-slate-900/50 rounded-xl h-[500px] px-6 py-8 ' >
         <div className='flex gap-2 justify-center '>
             <p className={'text-xl ' + playfairDisplay.className} > <span className='text-red-500 font-semibold'>F</span>riquently</p>
             <p className={'text-xl ' + playfairDisplay.className} > <span className='text-red-500 font-semibold'>A</span>ked</p>
             <p className={'text-xl ' + playfairDisplay.className} > <span className='text-red-500 font-semibold'>Q</span>uestions</p>
+            {selectedQuestions}
         </div>
         <div className='grid grid-cols-2 mt-10 gap-6 '>
-            {faqContent.map((element, index,)=>{
+            {/* {faqContent.map((element, index,)=>{
                 return(
                         <div key={index} >
                            <span className='text-green-500'>  {faqContent[index][0]} </span>
                            <span className='text-red-500'> {faqContent[index][1]} </span>
                         </div>
                 )
-            })}
-            
+            })} */}
+            <div className='border-2 border-white rounded-xl shadow-lg p-4 '>
+                <div className='flex items-center gap-2 '>
+                    <i className="fa-solid fa-chevron-down -rotate-90 transition-all duration-300 hover:rotate-0"></i>
+                    <span className='text-white'> What services do you offer? </span>
+                </div>
+                <span className='text-red-500 hidden '> all </span>
+            </div>
+            <div >
+                <span className='text-green-500'> ss?  </span>
+                <span className='text-red-500'> ss1!</span>
+            </div>
         </div>
     </div>
   )
