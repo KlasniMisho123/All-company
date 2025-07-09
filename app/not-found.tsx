d4a4645a6b7589ec30f5be11eb1095cc89c7d634
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-09 21:15:40 +0400

    faq commit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
       <h1 className="text-6xl font-bold mb-4">404</h1>
       <p className="text-2xl mb-2">Page Not Found</p>
       <p className="text-gray-400 mb-6">The page you’re looking for doesn’t exist or was moved.</p>
-      <Link href="/" className="px-6 py-2 bg-blue-600 hover:bg-blue-500 transition rounded-full text-white font-semibold cursor-pointer ">
+      <Link href="/" className="transition-all duration-300 px-6 py-2 bg-red-500/90 hover:bg-red-500/70  rounded-full text-white font-semibold cursor-pointer ">
         Go Back Home
       </Link>
     </div>
