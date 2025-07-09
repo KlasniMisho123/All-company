c5969dd761a98690758c3db76f2b8e353682187a
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-09 23:59:25 +0400

    all projects commit

--- components/ProjectsElement.tsx
+++ components/ProjectsElement.tsx
@@ -52,46 +52,23 @@ export default function ProjectsElement() {
             desc: "Clean, sustainable visual identity for an eco-conscious coffee brand.",
             date: "7/2/2025",
         },
-        {
-            imgSrc: "/nenesProject.jpg",
-            type: "Branding / UI Design",
-            title: "ChildFood Packaging",
-            desc: "Playful and safe packaging for a children's snack brand using vibrant pastel color schemes.",
-            date: "7/2/2025",
-        },
-        {
-            imgSrc: "/naturaleza.jpg",
-            type: "Nature Campaign",
-            title: "Naturaleza Posters",
-            desc: "Minimalistic and earthy poster series to promote ecological awareness through design.",
-            date: "2025-07-03",
-        },
-        {
-            imgSrc: "/naturaleza.jpg",
-            type: "Nature Campaign",
-            title: "Naturaleza Posters",
-            desc: "Minimalistic and earthy poster series to promote ecological awareness through design.",
-            date: "2025-07-03",
-        },
-        {
-            imgSrc: "/naturaleza.jpg",
-            type: "Nature Campaign",
-            title: "Naturaleza Posters",
-            desc: "Minimalistic and earthy poster series to promote ecological awareness through design.",
-            date: "2025-07-03",
-        },
+      
     ];
 
     function expandProjectDisplay() {
-        setIsExpanded(prev => !prev)
-
-        if(isExpanded) {
+        setIsExpanded(prev => {
+            const newExpanded = !prev;
+            
+            if (!newExpanded) {
             setTimeout(() => {
-                setProjectsMounted(prev => !prev);
+                setProjectsMounted(false);
             }, 1000);
-        } else {
-            setProjectsMounted(prev => !prev)
-        }
+            } else {
+            setProjectsMounted(true);
+            }
+
+            return newExpanded;
+        })
     }    
 
   return (
@@ -99,35 +76,26 @@ export default function ProjectsElement() {
         <div className={`text-2xl font-semibold relative px-6 py-2 border-2 rounded-t rounded-b-2xl inline-block ${urbanist.className}`}>
           <span className="relative z-10">Recent Projects</span>
         </div>
-        
-        <div className={'mt-4 w-full gap-6 grid grid-cols-3 overflow-hidden transition-all duration-500 ease-in-out ' + (isExpanded ? 'max-h-[2000px]' : 'max-h-[500px]') }>
-            {projects.slice(0,3).map((project, index)=>{
-                return(
-                    <RecentProjectsCard key={index}
-                        imgSrc={project.imgSrc}
-                        type={project.type}
-                        title={project.title}
-                        desc={project.desc}
-                        date={project.date}
-                    />
-                )
-            })}
+        <div>
+            <div className={'my-6 w-full gap-6 grid grid-cols-3 overflow-hidden transition-all duration-500 ease-in-out ' + (isExpanded ? 'max-h-[2000px]' : 'max-h-[500px]') }>
+                {projects.slice(0,3).map((project, index)=>{
+                    return(
+                        <RecentProjectsCard key={index} {...project} />
+                    )
+                })}
 
-        </div>
+            </div>
 
-        {projectsMounted && (<div className={'w-full gap-6 grid grid-cols-3 overflow-hidden transition-all duration-500 ease-in-out ' + (isExpanded ? 'max-h-[2000px]' : 'max-h-[0px]') }>
-           {projects.slice(3, projects.length).map((project, index)=>{
-                return(
-                    <RecentProjectsCard key={index}
-                        imgSrc={project.imgSrc}
-                        type={project.type}
-                        title={project.title}
-                        desc={project.desc}
-                        date={project.date}
-                    />
-                )
-            })}
-        </div>)}
+            <div
+                className={`w-full gap-6 grid grid-cols-3 overflow-hidden transition-all duration-500 ease-in-out
+                ${isExpanded ? 'max-h-[2000px] opacity-100' : 'max-h-0 opacity-90 pointer-events-none'}
+                `}
+            >
+                {projects.slice(3).map((project, index) => (
+                <RecentProjectsCard key={index} {...project} />
+                ))}
+            </div>
+            </div>
 
         <button
           className='text-center transition-all duration-200 font-semibold hover:opacity-75 cursor-pointer ' 
