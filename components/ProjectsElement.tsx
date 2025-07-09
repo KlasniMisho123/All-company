4c1473950a64272f1b19c734faab09ce2f2533f9
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-09 23:36:53 +0400

    all projects commit

--- components/ProjectsElement.tsx
+++ components/ProjectsElement.tsx
@@ -44,6 +44,41 @@ export default function ProjectsElement() {
             desc: "Minimalistic and earthy poster series to promote ecological awareness through design.",
             date: "2025-07-03",
         },
+        {
+            imgSrc: "/fight-club.jpg",
+            type: "Branding / UI Design",
+            title: "EcoBrew Identity",
+            desc: "Clean, sustainable visual identity for an eco-conscious coffee brand.",
+            date: "7/2/2025",
+        },
+        {
+            imgSrc: "/nenesProject.jpg",
+            type: "Branding / UI Design",
+            title: "ChildFood Packaging",
+            desc: "Playful and safe packaging for a children's snack brand using vibrant pastel color schemes.",
+            date: "7/2/2025",
+        },
+        {
+            imgSrc: "/naturaleza.jpg",
+            type: "Nature Campaign",
+            title: "Naturaleza Posters",
+            desc: "Minimalistic and earthy poster series to promote ecological awareness through design.",
+            date: "2025-07-03",
+        },
+        {
+            imgSrc: "/naturaleza.jpg",
+            type: "Nature Campaign",
+            title: "Naturaleza Posters",
+            desc: "Minimalistic and earthy poster series to promote ecological awareness through design.",
+            date: "2025-07-03",
+        },
+        {
+            imgSrc: "/naturaleza.jpg",
+            type: "Nature Campaign",
+            title: "Naturaleza Posters",
+            desc: "Minimalistic and earthy poster series to promote ecological awareness through design.",
+            date: "2025-07-03",
+        },
     ];
 
     function expandProjectDisplay() {
@@ -55,9 +90,9 @@ export default function ProjectsElement() {
         <div className={`text-2xl font-semibold relative px-6 py-2 border-2 rounded-t rounded-b-2xl inline-block ${urbanist.className}`}>
           <span className="relative z-10">Recent Projects</span>
         </div>
-
-        <div className={'mt-4 w-full gap-6 grid grid-cols-3 overflow-hidden transition-all duration-500 ease-in-out ' + (isExpanded? "min-h-0 " : "min-h-[40vh] ")}>
-            {(isExpanded? projects: projects.slice(0,3)).map((project, index)=>{
+        
+        <div className={'mt-4 w-full gap-6 grid grid-cols-3 overflow-hidden transition-all duration-500 ease-in-out ' + (isExpanded ? 'max-h-[2000px]' : 'max-h-[500px]') }>
+            {projects.slice(0,3).map((project, index)=>{
                 return(
                     <RecentProjectsCard key={index}
                         imgSrc={project.imgSrc}
@@ -68,51 +103,23 @@ export default function ProjectsElement() {
                     />
                 )
             })}
-          
 
-          
         </div>
 
-        {/* <div className={'w-full gap-6 grid grid-cols-3 overflow-hidden transition-all duration-500 ease-in-out ' + (isExpanded? "max-h-0 " : "h-max max-h-[")}>
-            <RecentProjectsCard 
-                imgSrc={"/fight-club.jpg"}
-                type={"Branding / UI Design"}
-                title={"EcoBrew Identity"}
-                desc={"Clean, sustainable visual identity for an eco-conscious coffee brand."}
-                date="7/2/2025"
-            />
-            <RecentProjectsCard 
-                imgSrc={"/nenesProject.jpg"}
-                type={"Branding / UI Design"}
-                title={"ChildFood Packaging"}
-                desc={"Playful and safe packaging for a children's snack brand using vibrant pastel color schemes."}
-                date="7/2/2025"
-            />
+        <div className={'w-full gap-6 grid grid-cols-3 overflow-hidden transition-all duration-500 ease-in-out ' + (isExpanded ? 'max-h-[2000px]' : 'max-h-[0px]') }>
+           {projects.slice(3, projects.length).map((project, index)=>{
+                return(
+                    <RecentProjectsCard key={index}
+                        imgSrc={project.imgSrc}
+                        type={project.type}
+                        title={project.title}
+                        desc={project.desc}
+                        date={project.date}
+                    />
+                )
+            })}
+        </div>
 
-            <RecentProjectsCard 
-                imgSrc="/naturaleza.jpg"
-                type="Nature Campaign"
-                title="Naturaleza Posters"
-                desc="Minimalistic and earthy poster series to promote ecological awareness through design."
-                date="2025-07-03"
-            />
-
-            <RecentProjectsCard 
-                imgSrc="/naturaleza.jpg"
-                type="Nature Campaign"
-                title="Naturaleza Posters"
-                desc="Minimalistic and earthy poster series to promote ecological awareness through design."
-                date="2025-07-03"
-            />
-
-            <RecentProjectsCard 
-                imgSrc="/naturaleza.jpg"
-                type="Nature Campaign"
-                title="Naturaleza Posters"
-                desc="Minimalistic and earthy poster series to promote ecological awareness through design."
-                date="2025-07-03"
-            />
-        </div> */}
         <button
           className='text-center transition-all duration-200 font-semibold hover:opacity-75 cursor-pointer ' 
           onClick={expandProjectDisplay}
