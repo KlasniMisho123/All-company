fdba59645014d5c76b868c7c80c80d57f8294422
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-09 19:58:11 +0400

    faq commit

--- components/FaqComponent.tsx
+++ components/FaqComponent.tsx
@@ -31,10 +31,39 @@ export default function FaqComponent() {
             <p className={'text-xl ' + playfairDisplay.className} > <span className='text-red-500 font-semibold'>Q</span>uestions</p>
             {selectedQuestions}
         </div>
+        <div>
+            <div className='inline'>1</div>
+            <div className='inline'>2</div>
+        </div>
         <div className='grid grid-cols-2 mt-10 gap-6 '>
             {faqContent.map((element, index,)=>{
+                // Left Side
                 return (
-                    <div key={index}  className='border-2 border-white rounded-xl shadow-lg p-4 cursor-pointer overflow-hidden '>
+                    <div key={index}  className='border-2 border-white rounded-xl shadow-lg p-4 cursor-pointer overflow-hidden inline'>
+                        <div className='flex items-center gap-4 '
+                        onClick={()=>{
+                            selectQuestions(index)
+                        }}
+                        >
+                            <i className={"fa-solid fa-chevron-right -rotate-90 transition-all duration-300 " + (selectedQuestions.includes(index)? " rotate-90 ": " rotate-0 ")}></i>
+                            <span className='text-white'> {faqContent[index][0]} </span>
+                        </div>
+                        <div className='overflow-hidden transition-all duration-500 ease-in-out'
+                            style={{
+                            maxHeight: selectedQuestions.includes(index) ? '100px' : '0px',
+                            opacity: selectedQuestions.includes(index) ? 1 : 0
+                            }}>
+                        <div className="px-4 mt-4 text-red-500 ">
+                            {faqContent[index][1]} 
+                        </div>
+                        </div>
+                    </div>
+                )
+            })}
+            {faqContent.map((element, index)=>{
+            // right Side
+                return (
+                    <div key={index}  className='border-2 border-white rounded-xl shadow-lg p-4 cursor-pointer overflow-hidden inline'>
                         <div className='flex items-center gap-4 '
                         onClick={()=>{
                             selectQuestions(index)
