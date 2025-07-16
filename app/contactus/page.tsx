34f2d128efbca720c2fc47de788cf8cbe2880e26
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-16 21:54:05 +0400

    contact us commit

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      {/* Header BAckground */}
+      <section>
+        Header
+      </section>
+      {/* Contact us Page */}
+      {/* Our Strong Sides */}
+      {/* next Steps */}
+    </div>
   )
 }
