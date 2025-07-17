dcdaf4496017e81225a78c01b1725aa49d29c477
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-17 16:16:22 +0400

    contact us commit

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -4,8 +4,28 @@ export default function page() {
   return (
     <div>
       {/* Header BAckground */}
-      <section>
-        Header
+      <section className='mx-auto flex flex-col items-center g w-[90%] my-16 '>
+        <div className='flex flex-col items-center gap-8'>
+          <div className="text-center space-y-4">
+            <h2 className="text-3xl font-bold">
+              It's Time to Design Something <span className="text-red-500">Exciting</span>
+            </h2>
+            <p className="text-gray-600 max-w-xl mx-auto">
+              Transform your ideas into visually compelling experiences. Let's create something extraordinary together.
+            </p>
+          </div>
+
+
+          <div>
+            <h2>
+              It's Time to Design Something 
+              <span> Exciting </span> 
+            </h2>
+            <p>P text - Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+          </div>
+        </div>
+        <div>Contact Form</div>
+        <div> Contact Cards </div>
       </section>
       {/* Contact us Form  - Questions etc*/}
       {/* Our Strong Sides */}
