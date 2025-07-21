ee4c2b251f273781f88d05292518ed79e1c8395e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-21 14:34:24 +0400

    theme roggle commit

--- components/ThemeToggleElement.tsx
+++ components/ThemeToggleElement.tsx
@@ -8,19 +8,19 @@ export default function ThemeToggleElement() {
     }
 
   return (
-        <div className=' hidden sm:flex border-2 border-[var(--error-color)] bg-blue-100 gap-6 justify-between py-1 px-2 rounded-full cursor-pointer transition-all duration-300 overflow-hidden '
+        <div className='relative hidden sm:flex items-center border-2 border-[var(--error-color)] bg-blue-100 gap-8 justify-between py-2 px-4 rounded-full cursor-pointer transition-all duration-300 overflow-hidden '
             onClick={()=>{
                 handleThemeToggle()
             }}
         >
-          {/* {isDay && (
-              <i className="fa-solid fa-sun text-3xl text-[var(--shiny-yellow)]"></i>
-          )} */}
+          {/* {isDay && ( */}
+              <i className={"fa-solid fa-sun text-2xl text-[var(--shiny-yellow)] transition-all duration-300 " + (!isDay? "-translate-x-[150%] translate-y-[150%] rotate-90 hidden" : "translate-x-0 translate-y-0 rotate-0" )}></i>
+          {/* )} */}
 
-          <div className={'h-8 w-8 z-10 rounded-full bg-[var(--primary-color)] transition-all duration-500  ' + (isDay? "translate-x-[150%]" : "translate-x-0" )}></div>
+          <div className={'absolute h-8 w-8 z-10 rounded-full bg-[var(--primary-color)] transition-all duration-500 z-10 ' + (isDay? "translate-x-[160%]" : "-translate-x-[25%]" )}></div>
 
           {/* {!isDay && ( */}
-              <i className={"fa-solid fa-moon text-3xl text-[var(--deep-lightpurple)] transition-all duration-300 " + (isDay? "translate-x-[100%] translate-y-[100%]" : "translate-x-0 translate-y-0" )}></i>
+              <i className={"fa-solid fa-moon text-2xl text-[var(--deep-lightpurple)] transition-all duration-300 " + (isDay? "translate-x-[150%] translate-y-[150%] rotate-90 hidden" : "translate-x-0 translate-y-0 rotate-0" )}></i>
           {/* )} */}
         </div>
   )
