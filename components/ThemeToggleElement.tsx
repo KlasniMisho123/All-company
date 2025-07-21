457ccf622a8cde44d8a01b2f0f14c03c34bf9de4
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-21 15:17:33 +0400

    theme toggle commit

--- components/ThemeToggleElement.tsx
+++ components/ThemeToggleElement.tsx
@@ -14,11 +14,13 @@ export default function ThemeToggleElement() {
                 handleThemeToggle()
             }}
         >
-            <i className={`fa-solid fa-sun text-2xl text-yellow-400 transition-all duration-300 ` + 
+            <i className={`fa-solid fa-sun text-2xl text-yellow-400 transition-all duration-300 shadow-[0_0_10px_2px_rgba(253,224,71,0.6)] rounded-full bg-blue-100 ` + 
                 (!isDay? "-translate-x-[150%] translate-y-[150%] rotate-90 hidden" : "-translate-x-2 translate-y-0 rotate-0" )}></i>
             <div className={'absolute h-8 w-8 z-10 rounded-full transition-all duration-500 z-10 ring-2  ' +
-                (isDay? "translate-x-[160%] bg-[var(--primary-color)] ring-yellow-400" : "-translate-x-[25%] bg-gray-100 ring-purple-300" )}></div>
-            <i className={"fa-solid fa-moon text-2xl text-purple-700 transition-all duration-300 " + 
+                (isDay? "translate-x-[160%] bg-[var(--primary-color)] ring-yellow-400  " :
+                 "-translate-x-[25%] bg-gray-100 ring-purple-300  ")}>
+            </div>
+            <i className={"fa-solid fa-moon text-2xl text-gray-600/80 transition-all duration-300 shadow-[0_0_10px_2px_rgba(156,163,175,0.4)] rounded-full bg-purple-200 " + 
                 (isDay? "translate-x-[150%] translate-y-[150%] rotate-90 hidden" : "translate-x-2 translate-y-0 rotate-0" )}></i>
         </div>
   )
