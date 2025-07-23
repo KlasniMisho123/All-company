8bc8eee41ceedc8e5f6b0b294cbf337401446be9
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-23 14:49:21 +0400

    reveal commit

--- components/animations/RevealSlide.tsx
+++ components/animations/RevealSlide.tsx
@@ -7,18 +7,42 @@ interface Props {
 }
 
 export const RevealSlide = ({ children, width = "fit-content"}:Props) => {
+   const ref = useRef(null)
+   const inView = useInView(ref, {once:true});
+
+   const mainControls = useAnimation();
+   const slideControls = useAnimation();
+
+   useEffect(() => {
+    if(inView) {
+        mainControls.start("visible")
+    }
+   }, [inView])
+
     return(
-        <div style={{ position: "relative", width, overflow:"hidden" }}>
+        <div ref={ref} style={{ position: "relative", width, overflow:"hidden" }}>
             <motion.div
                 variants={{
                     hidden: {opacity:0, y:75},
                     visible: {opacity:1, y:0},
                 }}
                 initial="hidden"
-                animate="visible"
-                transition={{}}
+                animate={mainControls}
+                transition={{ duration: 0.5, delay:0.25 }}
             >
-                {children}</motion.div>
+                {children}
+            </motion.div>
+            <motion.div
+                variants={{
+                    hidden: {left:0},
+                    visible: {left:"100%"},
+                }}
+                initial="hidden"
+                animate={slideControls}
+                transition={{ duration: 0.5, delay:0.25 }}
+            >
+                {children}
+            </motion.div>
         </div>
     );
 };
