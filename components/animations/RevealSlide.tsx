44bdaaf6656a4f96e511af24343f0b953975c2fb
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-23 14:33:18 +0400

    reveal commit

--- components/animations/RevealSlide.tsx
+++ components/animations/RevealSlide.tsx
@@ -9,7 +9,16 @@ interface Props {
 export const RevealSlide = ({ children, width = "fit-content"}:Props) => {
     return(
         <div style={{ position: "relative", width, overflow:"hidden" }}>
-            <div>{children}</div>
+            <motion.div
+                variants={{
+                    hidden: {opacity:0, y:75},
+                    visible: {opacity:1, y:0},
+                }}
+                initial="hidden"
+                animate="visible"
+                transition={{}}
+            >
+                {children}</motion.div>
         </div>
     );
 };
