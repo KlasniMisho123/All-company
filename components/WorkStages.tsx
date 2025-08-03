35057941fe7f7dc1d25a4f9f2c21275ea0b33d09
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-03 13:15:00 +0400

    work stages commit

--- components/WorkStages.tsx
+++ components/WorkStages.tsx
@@ -6,14 +6,14 @@ const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function WorkStages() {
   return (
-    <section className='flex flex-col justify-evenly gap-2 lg:gap-4 xl:gap-8 lg:flex-row mx-30 my-10 rounded-xl  p-4 py-10  '>
-      <div className='flex flex-col gap-4 '>
+    <section className='flex flex-col justify-evenly gap-2 lg:gap-4 xl:gap-8 lg:flex-row mx-4 sm:mx-8  md:mx-30 my-10 rounded-xl  p-4 py-10  '>
+      <div className='flex flex-col gap-4'>
         <RevealSlide width='100%' direction='up'> 
           <div className='my-4'>
-            <h4 className='text-2xl'> Work <span className='bg-gradient-to-r from-[var(--error-color)] to-[#1F2937] tracking-widest font-semibold px-1 '> Process </span> </h4>
-            <h2 className='text-4xl font-semibold '> Our business   <span className='text-[var(--error-color)]'> Process Road </span> </h2>
-              <p className='text-gray-100 mt-2 '>
-                  <span className='text-lg'> 🎨 </span> From the first pencil sketch to the final brand guidelines, our design process transforms raw ideas into iconic visual identities. 
+            <h4 className='text-xl sm:text-2xl truncate '> Work <span className='bg-gradient-to-r from-[var(--error-color)] to-[#1F2937] tracking-widest font-semibold px-1 '> Process </span> </h4>
+            <h2 className='text-4xl font-semibold '> <span className=' truncate'> Our business </span>  <span className='text-[var(--error-color)] truncate'> Process Road </span> </h2>
+              <p className='text-gray-100 mt-2 text-sm md:text-base line-clamp-7 '>
+                  <span className='text-lg '> 🎨 </span> From the first pencil sketch to the final brand guidelines, our design process transforms raw ideas into iconic visual identities. 
                   Each phase blends strategic thinking with artistic mastery—keeping you engaged at every turn, while crafting results that don&apos;t just meet briefs,
                   but redefine categories.
               </p>
@@ -23,39 +23,38 @@ export default function WorkStages() {
           <RevealSlide width='100%' direction='right'>
             <div className='flex items-center gap-4 transition-all transition-duration-300 bg-gray-600/50 p-3 rounded-lg shadow-md hover:shadow-none hover:bg-gray-600 group '>
               <i className="fa-solid fa-feather text-xl text-red-400/80 group-hover:bg-[var(--error-color)] transition-all transition-duration-300 group-hover:text-gray-300 rounded-full px-1.5 py-1.5 "></i>
-              <h4 className='text-sm'> We explore your vision, define goals, and shape the creative direction. </h4> 
+              <h4 className='text-xs md:text-sm line-clamp-2 '> We explore your vision, define goals, and shape the creative direction. </h4> 
             </div>
           </RevealSlide>
 
           <RevealSlide width='100%' direction='right'>
             <div className='flex items-center gap-4 transition-all transition-duration-300 bg-gray-600/50 p-3 rounded-lg shadow-md hover:shadow-none hover:bg-gray-600 group '>
               <i className="fa-solid fa-feather text-xl text-red-400/80 group-hover:bg-[var(--error-color)] transition-all transition-duration-300 group-hover:text-gray-300 rounded-full px-1.5 py-1.5"></i>
-              <h4 className='text-sm'> Initial visuals and interactive mockups are crafted for early feedback.</h4> 
+              <h4 className='text-xs md:text-sm line-clamp-2'> Initial visuals and interactive mockups are crafted for early feedback.</h4> 
             </div>
           </RevealSlide>
 
           <RevealSlide width='100%' direction='right'>
             <div className='flex items-center gap-4 transition-all transition-duration-300 bg-gray-600/50 p-3 rounded-lg shadow-md hover:shadow-none hover:bg-gray-600 group '>
               <i className="fa-solid fa-feather text-lg text-red-400/80 group-hover:bg-[var(--error-color)] transition-all transition-duration-300 group-hover:text-gray-300 rounded-full px-1.5 py-1.5 "></i>
-              <h4 className='text-sm'> We present the demo, gather insights, and fine-tune the design iteratively. </h4>
+              <h4 className='text-xs md:text-sm line-clamp-2'> We present the demo, gather insights, and fine-tune the design iteratively. </h4>
             </div>
           </RevealSlide>
 
           <RevealSlide width='100%' direction='right'>
             <div className='flex items-center gap-4 transition-all transition-duration-300 bg-gray-600/50 p-3 rounded-lg shadow-md hover:shadow-none hover:bg-gray-600 group '>
               <i className="fa-solid fa-feather text-xl text-red-400/80 group-hover:bg-[var(--error-color)] transition-all transition-duration-300 group-hover:text-gray-300 rounded-full px-1.5 py-1.5 "></i>
-              <h4 className='text-sm'> The final product is polished, deployed, and ready to make an impact. </h4> 
+              <h4 className='text-xs md:text-sm line-clamp-2'> The final product is polished, deployed, and ready to make an impact. </h4> 
             </div>
           </RevealSlide>
         </div>
-
-        
       </div>
-      <div className='flex flex-col items-center h-full gap-4 justify-start py-8 px-0 xl:px-8 md:justify-center md:gap-8 sm:flex-row md:items-start text-black '>
 
-        <div className='flex flex-col gap-4 sm:gap-8'>
+      <div className='flex flex-col items-center h-full gap-4 justify-start py-8 px-0 xl:px-8 md:justify-center md:gap-8 sm:flex-row md:items-start text-black mx-1 sm:mx-0 '>
+
+        <div className='flex flex-col gap-4 sm:gap-8 '>
           <RevealSlide width='100%' direction='right'>
-            <div className='flex flex-col p-4 py-12 bg-white gap-12 min-h-[325px] w-[350px] sm:w-[300px] rounded-md transition-all duration-300
+            <div className='flex flex-col p-4 py-12 bg-white gap-12 min-h-[325px] min-w-[250px] sm:w-[300px] rounded-md transition-all duration-300
               border-2 border-transparent shadow-[0_4px_12px_rgba(250,204,21,0.4)]
               hover:border-yellow-700 group overflow-hidden'>
               <div className='relative pt-8'>
@@ -69,7 +68,7 @@ export default function WorkStages() {
             </div>
           </RevealSlide>
           <RevealSlide width='100%' direction='right'>
-            <div className='relative  flex flex-col p-4 py-12 bg-white gap-12 min-h-[325px] w-[350px] sm:w-[300px] rounded-md transition-all duration-300 
+            <div className='relative flex flex-col p-4 py-12 bg-white gap-12 min-h-[325px] min-w-[250px] sm:w-[300px] rounded-md transition-all duration-300 
               border-2 border-transparent shadow-[0_4px_12px_rgba(168,85,247,0.4)]
               hover:border-purple-700 group overflow-hidden'>
               <div className='relative pt-8'> 
@@ -85,7 +84,7 @@ export default function WorkStages() {
         </div>
           <div className='flex flex-col gap-4 sm:gap-8 mt-8'>
             <RevealSlide width='100%' direction='left'>
-              <div className='relative flex flex-col  p-4 py-12 bg-white gap-12 min-h-[325px] w-[350px] sm:w-[300px] rounded-md transition-all duration-300
+              <div className='relative flex flex-col  p-4 py-12 bg-white gap-12 min-h-[325px] min-w-[250px] sm:w-[300px] rounded-md transition-all duration-300
                 border-2 border-transparent shadow-[0_4px_12px_rgba(59,130,246,0.4)]
                 hover:border-blue-700 group overflow-hidden'>
                   <div className='relative pt-8'> 
@@ -100,7 +99,7 @@ export default function WorkStages() {
                 </div>
               </RevealSlide>
               <RevealSlide width='100%' direction='left'>
-                <div className='flex flex-col p-4 py-12 bg-white gap-12 min-h-[325px] w-[350px] sm:w-[300px] rounded-md transition-all duration-300
+                <div className='flex flex-col p-4 py-12 bg-white gap-12 min-h-[325px] min-w-[250px] sm:w-[300px] rounded-md transition-all duration-300
                 border-2 border-transparent shadow-[0_4px_12px_rgba(239,68,68,0.4)] 
                 hover:border-red-700 group overflow-hidden group'>
                   <div className='relative pt-8'> 
